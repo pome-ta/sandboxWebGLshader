5f9968203c56ea1fa3e22499ed478f2312f47135
pome-ta <pometarou93@gmail.com>
2022-07-26 00:22:43 +0900

    test

---
+++ docs/js/app.js
@@ -0,0 +1,185 @@
+'use strict';
+
+let run;
+let uniLocation = new Array();
+let mx = 0.0;
+let my = 0.0;
+
+let time = 0.0;
+const FPS = 24;
+const frameTime = 1 / FPS;
+let prevTimestamp = 0;
+
+let WIDTH, HEIGHT;
+
+const canvasDiv = document.createElement('div');
+canvasDiv.style.width = '100%';
+const cxtCanvas = document.createElement('canvas');
+cxtCanvas.style.width = '100%';
+canvasDiv.appendChild(cxtCanvas);
+document.body.appendChild(canvasDiv);
+
+const gl = cxtCanvas.getContext('webgl');
+const prg = create_program(create_shader('vs'), create_shader('fs'));
+run = prg ? prg : null;
+uniLocation[0] = gl.getUniformLocation(prg, 'time');
+uniLocation[1] = gl.getUniformLocation(prg, 'mouse');
+uniLocation[2] = gl.getUniformLocation(prg, 'rezolution');
+
+const position = [
+  -1.0, 1.0, 0.0, 1.0, 1.0, 0.0, -1.0, -1.0, 0.0, 1.0, -1.0, 0.0,
+];
+const index = [0, 2, 1, 1, 2, 3];
+
+const vPosition = create_vbo(position);
+const vIndex = create_ibo(index);
+const vAttLocation = gl.getAttribLocation(prg, 'position');
+gl.bindBuffer(gl.ARRAY_BUFFER, vPosition);
+gl.enableVertexAttribArray(vAttLocation);
+gl.vertexAttribPointer(vAttLocation, 3, gl.FLOAT, false, 0, 0);
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, vIndex);
+
+gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+function render(timestamp) {
+  const elapsed = (timestamp - prevTimestamp) / 1000;
+  if (elapsed <= frameTime) {
+    requestAnimationFrame(render);
+    return;
+  }
+  prevTimestamp = timestamp;
+  time += prevTimestamp;
+  // カラーバッファをクリア
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // uniform 関連
+  gl.uniform1f(uniLocation[0], time);
+  gl.uniform2fv(uniLocation[1], [mx, my]);
+  gl.uniform2fv(uniLocation[2], [WIDTH, HEIGHT]);
+
+  // 描画
+  gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+  gl.flush();
+
+  // 再帰
+  requestAnimationFrame(render);
+}
+
+function initCanvas() {
+  cxtCanvas.width = canvasDiv.clientWidth;
+  cxtCanvas.height = canvasDiv.clientWidth;
+  WIDTH = canvasDiv.width;
+  HEIGHT = cxtCanvas.height;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  initCanvas();
+  render();
+});
+
+// シェーダを生成する関数
+function create_shader(id) {
+  // シェーダを格納する変数
+  let shader;
+
+  // HTMLからscriptタグへの参照を取得
+  const scriptElement = document.getElementById(id);
+
+  // scriptタグが存在しない場合は抜ける
+  if (!scriptElement) {
+    return;
+  }
+
+  // scriptタグのtype属性をチェック
+  switch (scriptElement.type) {
+    // 頂点シェーダの場合
+    case 'x-shader/x-vertex':
+      shader = gl.createShader(gl.VERTEX_SHADER);
+      break;
+
+    // フラグメントシェーダの場合
+    case 'x-shader/x-fragment':
+      shader = gl.createShader(gl.FRAGMENT_SHADER);
+      break;
+
+    default:
+      return;
+  }
+
+  // 生成されたシェーダにソースを割り当てる
+  gl.shaderSource(shader, scriptElement.text);
+
+  // シェーダをコンパイルする
+  gl.compileShader(shader);
+
+  // シェーダが正しくコンパイルされたかチェック
+  if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    // 成功していたらシェーダを返して終了
+    return shader;
+  } else {
+    // 失敗していたらエラーログをアラートしコンソールに出力
+    // alert(gl.getShaderInfoLog(shader));
+    console.log(gl.getShaderInfoLog(shader));
+  }
+}
+
+// プログラムオブジェクトを生成しシェーダをリンクする関数
+function create_program(vs, fs) {
+  // プログラムオブジェクトの生成
+  const program = gl.createProgram();
+
+  // プログラムオブジェクトにシェーダを割り当てる
+  gl.attachShader(program, vs);
+  gl.attachShader(program, fs);
+
+  // シェーダをリンク
+  gl.linkProgram(program);
+
+  // シェーダのリンクが正しく行なわれたかチェック
+  if (gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    // 成功していたらプログラムオブジェクトを有効にする
+    gl.useProgram(program);
+
+    // プログラムオブジェクトを返して終了
+    return program;
+  } else {
+    // 失敗していたら NULL を返す
+    return null;
+  }
+}
+
+// VBOを生成する関数
+function create_vbo(data) {
+  // バッファオブジェクトの生成
+  const vbo = gl.createBuffer();
+
+  // バッファをバインドする
+  gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+
+  // バッファにデータをセット
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+
+  // バッファのバインドを無効化
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+  // 生成した VBO を返して終了
+  return vbo;
+}
+
+// IBOを生成する関数
+function create_ibo(data) {
+  // バッファオブジェクトの生成
+  const ibo = gl.createBuffer();
+
+  // バッファをバインドする
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+
+  // バッファにデータをセット
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array(data), gl.STATIC_DRAW);
+
+  // バッファのバインドを無効化
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+  // 生成したIBOを返して終了
+  return ibo;
+}
