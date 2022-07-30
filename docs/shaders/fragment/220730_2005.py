8846b3d9760d4cc49d400279af546c4b4801acad
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-07-30 20:19:47 +0900

    getNormal

--- docs/shaders/fragment/220730_2005.py
+++ docs/shaders/fragment/220730_2005.py
@@ -9,21 +9,10 @@ out vec4 fragmentColor;
 
 
 const float sphereSize = 1.0;
-const vec3 lightDir = vec3(-0.577, 0.577, 0.577);
 
 float distanceFunc(vec3 p) {
   return length(p) - sphereSize;
 }
-
-vec3 getNormal(vec3 p) {
-  float d = 0.0001;
-  return normalize(vec3(
-    distanceFunc(p + vec3(  d, 0.0, 0.0)) - distanceFunc(p + vec3( -d, 0.0, 0.0)),
-    distanceFunc(p + vec3(0.0,   d, 0.0)) - distanceFunc(p + vec3(0.0,  -d, 0.0)),
-    distanceFunc(p + vec3(0.0, 0.0,   d)) - distanceFunc(p + vec3(0.0, 0.0,  -d))
-  ));
-}
-
 
 void main(void) {
   vec2 p = (gl_FragCoord.xy * 2.0 - resolution) / min(resolution.x, resolution.y);
@@ -44,7 +33,7 @@ void main(void) {
   /* marching loop */
   float distance = 0.0;  // レイとオブジェクト間の最短距離
   float rayAddLength = 0.0;  // レイに継ぎ足す長さ
-  vec3 rayPos = cameraPos;  // レイの先端位置
+  vec3  rayPos = cameraPos;  // レイの先端位置
   
   for(int i = 0; i < 16; i++) {
     distance = distanceFunc(rayPos);
@@ -55,9 +44,7 @@ void main(void) {
   /* hit check */
   vec4 outColor;
   if(abs(distance) < 0.001) {
-    vec3 normal = getNormal(rayPos);
-    float diff = clamp(dot(lightDir, normal), 0.1, 1.0);
-    outColor = vec4(vec3(diff), 1.0);
+    outColor = vec4(vec3(1.0), 1.0);
   } else {
     outColor = vec4(vec3(0.0), 1.0);
   }
