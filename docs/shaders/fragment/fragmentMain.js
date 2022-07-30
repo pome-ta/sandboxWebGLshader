d8c20b903e1da42d5ccad6704ef4b2507176a7c9
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-07-31 00:11:06 +0900

    up

--- docs/shaders/fragment/fragmentMain.js
+++ docs/shaders/fragment/fragmentMain.js
@@ -8,6 +8,11 @@ uniform vec2 mouse;
 out vec4 fragmentColor;
 
 
+
+const float PI = acos(-1.0);
+const float angle = 90.0;
+const float fov = angle * 0.5 * PI / 180.0;
+
 const float sphereSize = 1.0;
 const vec3 lightDir = vec3(-0.577, 0.577, 0.577);
 
@@ -16,7 +21,7 @@ float distanceFunc(vec3 p) {
 }
 
 vec3 getNormal(vec3 p) {
-  float d = 0.0001;
+  float d = 0.00001;
   return normalize(vec3(
     distanceFunc(p + vec3(  d, 0.0, 0.0)) - distanceFunc(p + vec3( -d, 0.0, 0.0)),
     distanceFunc(p + vec3(0.0,   d, 0.0)) - distanceFunc(p + vec3(0.0,  -d, 0.0)),
@@ -30,16 +35,10 @@ void main(void) {
   
   /* camera */
   vec3 cameraPos = vec3(0.0, 0.0, 2.0);
-  vec3 cameraDir = vec3(0.0, 0.0, -1.0);
-  vec3 cameraUp  = vec3(0.0, 1.0, 0.0);
-  vec3 cameraSide = cross(cameraDir, cameraUp);
-  
-  float targetDepth = 1.0;
   
   /* ray */
-  vec3 ray = normalize(cameraSide * p.x
-                     + cameraUp * p.y
-                     + cameraDir * targetDepth);
+  //vec3 ray = normalize(cameraSide * p.x + cameraUp * p.y + cameraDir * targetDepth);
+  vec3 ray = normalize(vec3(sin(fov) * p.x, sin(fov) * p.y, -cos(fov)));
  
   /* marching loop */
   float distance = 0.0;  // レイとオブジェクト間の最短距離
@@ -57,7 +56,7 @@ void main(void) {
   if(abs(distance) < 0.001) {
     vec3 normal = getNormal(rayPos);
     float diff = clamp(dot(lightDir, normal), 0.1, 1.0);
-    outColor = vec4(vec3(diff), 1.0);
+    outColor = vec4(vec3(normal), 1.0);
   } else {
     outColor = vec4(vec3(0.0), 1.0);
   }
