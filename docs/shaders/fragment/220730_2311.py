f3f7a31771b14621f0bef48e926aced69d7d7766
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-07-31 00:04:09 +0900

    up

---
+++ docs/shaders/fragment/220730_2311.py
@@ -0,0 +1,66 @@
+#version 300 es
+precision highp float;
+
+uniform float time;
+uniform vec2 resolution;
+uniform vec2 mouse;
+
+out vec4 fragmentColor;
+
+
+
+const float PI = acos(-1.0);
+const float angle = 90.0;
+const float fov = angle * 0.5 * PI / 180.0;
+
+const float sphereSize = 1.0;
+const vec3 lightDir = vec3(-0.577, 0.577, 0.577);
+
+float distanceFunc(vec3 p) {
+  return length(p) - sphereSize;
+}
+
+vec3 getNormal(vec3 p) {
+  float d = 0.00001;
+  return normalize(vec3(
+    distanceFunc(p + vec3(  d, 0.0, 0.0)) - distanceFunc(p + vec3( -d, 0.0, 0.0)),
+    distanceFunc(p + vec3(0.0,   d, 0.0)) - distanceFunc(p + vec3(0.0,  -d, 0.0)),
+    distanceFunc(p + vec3(0.0, 0.0,   d)) - distanceFunc(p + vec3(0.0, 0.0,  -d))
+  ));
+}
+
+
+void main(void) {
+  vec2 p = (gl_FragCoord.xy * 2.0 - resolution) / min(resolution.x, resolution.y);
+  
+  /* camera */
+  vec3 cameraPos = vec3(0.0, 0.0, 2.0);
+  
+  /* ray */
+  //vec3 ray = normalize(cameraSide * p.x + cameraUp * p.y + cameraDir * targetDepth);
+  vec3 ray = normalize(vec3(sin(fov) * p.x, sin(fov) * p.y, -cos(fov)));
+ 
+  /* marching loop */
+  float distance = 0.0;  // レイとオブジェクト間の最短距離
+  float rayAddLength = 0.0;  // レイに継ぎ足す長さ
+  vec3 rayPos = cameraPos;  // レイの先端位置
+  
+  for(int i = 0; i < 16; i++) {
+    distance = distanceFunc(rayPos);
+    rayAddLength += distance;
+    rayPos = cameraPos + ray * rayAddLength;
+  }
+    
+  /* hit check */
+  vec4 outColor;
+  if(abs(distance) < 0.001) {
+    vec3 normal = getNormal(rayPos);
+    float diff = clamp(dot(lightDir, normal), 0.1, 1.0);
+    outColor = vec4(vec3(normal), 1.0);
+  } else {
+    outColor = vec4(vec3(0.0), 1.0);
+  }
+  
+  fragmentColor = outColor;
+}
+
