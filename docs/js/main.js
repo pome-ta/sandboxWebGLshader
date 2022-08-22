3317296b5912eb59f4ab9c54cb87c79c23998575
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-08-22 17:06:57 +0900

    up

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,10 +2,20 @@ import { Fragmen } from './fragmen.js';
 
 const canvasDiv = document.createElement('div');
 const cxtCanvas = document.createElement('canvas');
+canvasDiv.style.width = '100%';
+canvasDiv.style.height = '100%';
+// canvasDiv.style.height = `${document.documentElement.clientHeight}px`;
+console.log(canvasDiv.clientHeight);
+// cxtCanvas.width = `${document.documentElement.clientWidth}px`;
+// cxtCanvas.height = `${document.documentElement.clientHeight}px`;
+cxtCanvas.style.width = '100%';
+cxtCanvas.style.height = '100%';
+
 canvasDiv.appendChild(cxtCanvas);
 document.body.appendChild(canvasDiv);
-canvasDiv.style.width = '100%';
-cxtCanvas.style.width = '100%';
+
+let currentMode = Fragmen.MODE_CLASSIC; // 現在の Fragmen モード
+let currentSource = ''; // 直近のソースコード
 
 // fragmen.js 用のオプションの雛形
 const FRAGMEN_OPTION = {
@@ -17,3 +27,22 @@ const FRAGMEN_OPTION = {
 };
 
 const fragmenDefaultSource = Fragmen.DEFAULT_SOURCE;
+currentMode = Fragmen.MODE_CLASSIC;
+currentSource = fragmenDefaultSource[currentMode];
+// メインとなる fragmen のインスタンス
+const option = Object.assign(FRAGMEN_OPTION, {
+  target: cxtCanvas,
+  eventTarget: window,
+});
+const fragmen = new Fragmen(option);
+fragmen.mode = currentMode;
+fragmen.render(currentSource);
+
+window.addEventListener('DOMContentLoaded', () => {
+  // cxtCanvas.width = `${canvasDiv.clientWidth}`;
+  // cxtCanvas.height = `${canvasDiv.clientHeight}`;
+});
+window.addEventListener('resize', () => {
+  // cxtCanvas.width = `${canvasDiv.clientWidth}`;
+  // cxtCanvas.height = `${canvasDiv.clientHeight}`;
+});
