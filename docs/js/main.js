1f95424074ee68c44898f3456190b281caaede78
pome-ta <pometarou93@gmail.com>
2022-08-23 00:28:08 +0900

    set canvas

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,35 +1,25 @@
 import { Fragmen } from './fragmen.js';
 
-let canvasDiv, cxtCanvas;
+let wrapDiv, canvasDiv, cxtCanvas;
 
-function createCanvas() {
-  //document.body.style.backgroundColor = 'dimgray';
-  //document.body.style.backgroundColor = '#232323';
+(() => {
+  wrapDiv = document.createElement('div');
   canvasDiv = document.createElement('div');
-  canvasDiv.style.overflow = 'hidden';
   cxtCanvas = document.createElement('canvas');
-  canvasDiv.appendChild(cxtCanvas);
-  document.body.appendChild(canvasDiv);
-  // canvasDiv.style.width = '100%';
+  document.body
+    .appendChild(wrapDiv)
+    .appendChild(canvasDiv)
+    .appendChild(cxtCanvas);
+
+  wrapDiv.style.height = '100%';
+  canvasDiv.style.overflow = 'hidden';
   canvasDiv.style.height = '100%';
-  // cxtCanvas.style.width = '100%';
   cxtCanvas.style.width = '100%';
   cxtCanvas.style.height = '100%';
-  //cxtCanvas.style.height = 'auto';
-}
-
-function initCanvasSize() {
-  // const oneSide = Math.min(canvasDiv.clientWidth, canvasDiv.clientHeight);
-  const oneSide = canvasDiv.clientWidth;
-  cxtCanvas.width = oneSide;
-  cxtCanvas.height = oneSide;
-  //cxtCanvas.height = canvasDiv.clientWidth;
-  //canvasW = cxtCanvas.width;
-  //canvasH = cxtCanvas.height;
-}
-
-createCanvas();
-// initCanvasSize();
+  let { width, height } = wrapDiv.getBoundingClientRect();
+  cxtCanvas.width = width;
+  canvasDiv.height = height;
+})();
 
 let currentMode = Fragmen.MODE_CLASSIC; // 現在の Fragmen モード
 let currentSource = ''; // 直近のソースコード
@@ -54,27 +44,3 @@ const option = Object.assign(FRAGMEN_OPTION, {
 const fragmen = new Fragmen(option);
 fragmen.mode = currentMode;
 fragmen.render(currentSource);
-
-cxtCanvas.addEventListener('touchmove', (event) => {
-  event.preventDefault();
-});
-
-/*
-window.addEventListener('DOMContentLoaded', () => {
-  // cxtCanvas.width = `${canvasDiv.clientWidth}`;
-  // cxtCanvas.height = `${canvasDiv.clientHeight}`;
-  canvasDiv.style.width = '100%';
-  canvasDiv.style.height = '100%';
-//canvasDiv.style.height = `${document.documentElement.clientHeight}px`;
-cxtCanvas.width = `${canvasDiv.clientWidth}px`;
-cxtCanvas.height = `${canvasDiv.clientHeight}px`;
-//cxtCanvas.style.width = `${canvasDiv.clientWidth}px`;
-//cxtCanvas.style.height = `${canvasDiv.clientHeight}px`;
-//fragmen.render(currentSource);
-});
-window.addEventListener('resize', () => {
-  // cxtCanvas.width = `${canvasDiv.clientWidth}`;
-  // cxtCanvas.height = `${canvasDiv.clientHeight}`;
-});
-
-*/
