f2b295f312106f947b3f104a90ccc04b7c1413ba
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-08-22 15:55:56 +0900

    up

---
+++ docs/js/main.js
@@ -0,0 +1,10 @@
+import {Fragmen} from './fragmen.js';
+
+// fragmen.js 用のオプションの雛形
+const FRAGMEN_OPTION = {
+    target: null,
+    eventTarget: null,
+    mouse: true,
+    resize: true,
+    escape: false
+}
