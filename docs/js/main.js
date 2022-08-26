b829ee519b094ac982ad33aaf76839f8f0c110c4
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-08-27 01:15:00 +0900

    up

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,68 +1,7 @@
 import { Fragmen } from './fragmen.js';
 import { wrapDiv, canvasDiv, message, modeSelect, editor } from './setDOMs.js';
 
-/*
-function createElements(tags) {
-  return tags.map((tag) => document.createElement(tag));
-}
 
-let latestStatus = 'success';
-const tagArray = ['main', 'div', 'div', 'select', 'div'];
-
-const modeOptions = [
-  'classic',
-  'geek',
-  'geeker',
-  'geekest',
-  'classic (300 es)',
-  'geek (300 es)',
-  'geeker (300 es)',
-  'geekest (300 es)',
-  'classic (MRT)',
-  'geek (MRT)',
-  'geeker (MRT)',
-  'geekest (MRT)',
-];
-
-let wrapDiv, canvasDiv, message, modeSelect, editor;
-(() => {
-  [wrapDiv, canvasDiv, message, modeSelect, editor] = createElements(tagArray);
-
-  modeSelect.style.width = '100%';
-  modeSelect.style.height = '2rem';
-  modeOptions.forEach((option, index) => {
-    const optionElement = document.createElement('option');
-    optionElement.value = index;
-    optionElement.text = option;
-    modeSelect.appendChild(optionElement);
-  });
-
-  
-  editor.style.fontFamily = 'monospace';
-  editor.style.fontSize = '0.8rem';
-
-  document.body.appendChild(wrapDiv);
-  wrapDiv.appendChild(modeSelect);
-  wrapDiv.appendChild(message);
-  wrapDiv.appendChild(editor);
-  wrapDiv.appendChild(canvasDiv);
-
-  //wrapDiv.style.display = 'grid';
-  //wrapDiv.gridTemplateRows = 'auto 1fr';
-  message.style.height = '2rem';
-  message.style.fontFamily = 'monospace';
-  message.textContent = ' ● ready';
-
-  wrapDiv.style.height = '100%';
-  canvasDiv.style.overflow = 'hidden';
-  canvasDiv.style.height = '100%';
-
-  // todo: setting fragmen glCanvas size
-  const { width, height } = wrapDiv.getBoundingClientRect();
-  canvasDiv.width = width;
-  canvasDiv.height = height;
-})();
-*/
 let currentMode = Fragmen.MODE_CLASSIC; // 現在の Fragmen モード
 let currentSource = ''; // 直近のソースコード
 
